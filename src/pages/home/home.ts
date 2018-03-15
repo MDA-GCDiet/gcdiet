937f83d27790f2ee6717f57eeff13d23a20e05f8
Jose Carlos <josecanib@gmail.com>
2018-03-15 20:34:34 +0000

    Navegación entre Home y Recipes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {RecipeDetailPage} from "../recipe-detail/recipe-detail";
+import {RecipesPage} from "../recipes/recipes";
 
 @Component({
   selector: 'page-home',
@@ -9,6 +11,11 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+
+  navRecipes(){
+    this.navCtrl.push(RecipesPage);
   }
 
 }
