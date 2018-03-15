4486972ddbe60e26d372f31c6983aa5e8744e30d
d4v1d-MR <david_mr94@hotmail.com>
2018-03-15 23:02:27 +0000

    creating access to database. Reading data

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {RecipeDetailPage} from "../recipe-detail/recipe-detail";
+import {DbApiService} from "../../shared/db-api.service";
 
 /**
  * Generated class for the RecipesPage page.
@@ -16,13 +17,26 @@ import {RecipeDetailPage} from "../recipe-detail/recipe-detail";
 })
 export class RecipesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  recipes = [];
+  fruits = [];
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private dbapi: DbApiService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad RecipesPage');
+    this.dbapi.getRecipes().subscribe(
+      (data) => this.recipes = data
+    );
   }
 
+
+  viewFruits(){
+    this.dbapi.getFruits().subscribe(
+      (data) => this.fruits = data
+    );
+  }
 
   navRecipeDetail(recipe){
     this.navCtrl.push(RecipeDetailPage, recipe);
